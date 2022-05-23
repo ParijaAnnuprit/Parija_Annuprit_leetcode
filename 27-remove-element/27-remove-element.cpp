bb1574eb5ca72d6a19f9b9cb09b8c24ccdb2675c
Parija Annuprit <71126167+ParijaAnnuprit@users.noreply.github.com>
2022-05-23 19:02:54 +0530

    Time: 0 ms (100.00%), Space: 8.9 MB (37.06%) - LeetHub

---
+++ 27-remove-element/27-remove-element.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int removeElement(vector<int>& nums, int val) {
+        int n = nums.size();
+//this gives wrong answer i.e. [0,1,2,2,3,0,4,2] == output [0,1,2,3,0,4] ==exp [0,1,4,0,3]
+        // for(auto i= nums.begin(); i<nums.end(); ++i){
+        //     if(*i == val)
+        //         nums.erase(i);
+        // }
+        // int k = nums.size();
+        // return k;
+        int i =0;
+        int start = 0;
+        while(i<n){
+            if(nums[i]== val)
+                i++;
+            else
+            {nums[start] = nums[i];
+            start++;
+            i++;}
+            }
+        return start;
+    }
+};
