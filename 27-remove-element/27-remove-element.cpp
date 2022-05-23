0da021de7cb1893d8612e637e10326c072eae349
Parija Annuprit <71126167+ParijaAnnuprit@users.noreply.github.com>
2022-05-23 19:23:28 +0530

    Time: 8 ms (9.45%), Space: 8.6 MB (92.78%) - LeetHub

--- 27-remove-element/27-remove-element.cpp
+++ 27-remove-element/27-remove-element.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     int removeElement(vector<int>& nums, int val) {
         int n = nums.size();
-//this gives wrong answer i.e. [0,1,2,2,3,0,4,2] == output [0,1,2,3,0,4] ==exp [0,1,4,0,3]
+// this gives wrong answer i.e. [0,1,2,2,3,0,4,2] == output [0,1,2,3,0,4] ==exp [0,1,4,0,3]
         // for(auto i= nums.begin(); i<nums.end(); ++i){
         //     if(*i == val)
         //         nums.erase(i);
