a0afdac13abf552fc408ab0614d6795a75f268ee
Parija Annuprit <71126167+ParijaAnnuprit@users.noreply.github.com>
2022-06-02 18:55:34 +0530

    Time: 14 ms (21.59%), Space: 5.9 MB (72.18%) - LeetHub

---
+++ 441-arranging-coins/441-arranging-coins.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int arrangeCoins(int n) {
+        long l = 1;
+        long h = n;
+        long m,curr;
+        // if(n == 1)
+        //     return n;
+        while(l<=h){
+            m = l+ ((h-l)/2);
+            curr = m*(m+1)/2;
+            if((curr < n && ((m+1)*(m+2))/2 > n) || (curr == n) )
+                return int(m);
+            if(curr < n)
+                l = m+1;
+            else if(curr > n)
+                h = m-1;
+            
+        }
+        return -1;
+    }
+};
