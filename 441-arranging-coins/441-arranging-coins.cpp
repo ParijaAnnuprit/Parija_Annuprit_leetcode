67d0f8e72f238460bcaff8d8279d4101c03d28ff
Parija Annuprit <71126167+ParijaAnnuprit@users.noreply.github.com>
2022-06-02 19:13:26 +0530

    Time: 3 ms (78.39%), Space: 6 MB (27.40%) - LeetHub

--- 441-arranging-coins/441-arranging-coins.cpp
+++ 441-arranging-coins/441-arranging-coins.cpp
@@ -11,7 +11,7 @@ public:
             curr = m*(m+1)/2;
             if((curr < n && ((m+1)*(m+2))/2 > n) || (curr == n) )
                 return int(m);
-            if(curr < n)
+            else if(curr < n)
                 l = m+1;
             else if(curr > n)
                 h = m-1;
