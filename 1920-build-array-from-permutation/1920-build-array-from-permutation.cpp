0f043f42a4f7b4f25856bd949678f86b3ba39f50
Parija Annuprit <71126167+ParijaAnnuprit@users.noreply.github.com>
2022-06-04 22:51:51 +0530

    Time: 15 ms (83.62%), Space: 17 MB (17.19%) - LeetHub

--- 1920-build-array-from-permutation/1920-build-array-from-permutation.cpp
+++ 1920-build-array-from-permutation/1920-build-array-from-permutation.cpp
@@ -1,11 +1,20 @@
+#include<vector>
+#include<iterator>
 class Solution {
 public:
     vector<int> buildArray(vector<int>& nums) {
-        vector<int>arr;
         int n = nums.size();
-        for(int i=0;i<n;i++){
-            arr.push_back(nums[nums[i]]);
+        for(int i=0; i<n; i++){
+            nums.push_back(nums[nums[i]]);
         }
-        return arr;
+        nums.erase(nums.begin(), nums.begin()+(n));
+        return nums;
     }
 };
+//         brute force approach TC-O(n) SC-O(n)
+        // vector<int>arr;
+        // int n = nums.size();
+        // for(int i=0;i<n;i++){
+        //     arr.push_back(nums[nums[i]]);
+        // }
+        // return arr;
