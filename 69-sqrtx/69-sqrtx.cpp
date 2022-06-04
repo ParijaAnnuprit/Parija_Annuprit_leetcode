f92b936c6af588bb83f8083e257231812bef5b7e
Parija Annuprit <71126167+ParijaAnnuprit@users.noreply.github.com>
2022-06-04 12:10:59 +0530

    Time: 0 ms (100.00%), Space: 5.8 MB (74.59%) - LeetHub

--- 69-sqrtx/69-sqrtx.cpp
+++ 69-sqrtx/69-sqrtx.cpp
@@ -1,13 +1,37 @@
 class Solution {
 public:
+//     binary search TC- O(n^2) SC- O(1)
     int mySqrt(int x) {
-        if(x==0 || x==1)
+        int l = 0;
+        int h = x;
+        if(x<2)
             return x;
-        for(long long int i=0; i<=x; i++){
-            long long int y = i*i;
-            if(y>x)
-                return i-1;
+        while(l<=h){
+            long long m = l + ((h-l)/2);
+            long long int temp = m*m;
+            if(temp>x)
+                h = m-1;
+            else if(temp<x)
+                if((m+1)*(m+1)>x)
+                    return m;
+                else 
+                    l = m+1;
+            else if(temp == x)
+                return m;
         }
         return 0;
     }
 };
+
+
+//     brute force TC- O(n^2) SC- O(1)
+    // int mySqrt(int x) {
+    //     if(x==0 || x==1)
+    //         return x;
+    //     for(long long int i=0; i<=x; i++){
+    //         long long int y = i*i;
+    //         if(y>x)
+    //             return i-1;
+    //     }
+    //     return 0;
+    // }
