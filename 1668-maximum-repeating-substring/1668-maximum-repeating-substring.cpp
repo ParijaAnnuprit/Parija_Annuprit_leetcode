84f5e052ba19e671c50396019fa75e9c0210a24b
Parija Annuprit <71126167+ParijaAnnuprit@users.noreply.github.com>
2022-06-09 21:28:31 +0530

    Time: 5 ms (23.03%), Space: 6.2 MB (89.70%) - LeetHub

--- 1668-maximum-repeating-substring/1668-maximum-repeating-substring.cpp
+++ 1668-maximum-repeating-substring/1668-maximum-repeating-substring.cpp
@@ -1,36 +1,39 @@
-// class Solution {
-// public:
-//     int maxRepeating(string sequence, string word) {
-//         int n = sequence.length();
-//         int m = word.length();
-//         int k = 0;
-//         if(m>n)
-//             return 0;
-//         else if(n==m && sequence==word)
-//             return 1;
-//         int i=0;
-//         for(int i=0; i<n; i++){
-//         while(i<n && sequence.substr(i,m)== word){
-//             k++;
-//             i+=m;
-//         }
-        
-//         return k++;
-//     }
-// };
 class Solution {
 public:
-    int maxRepeating(string s, string w) {
-        int mx=0,nw=w.size(),n=s.size();
-        if(nw>n) return 0;
-        for(int i=0;i<n;i++){
-            int k=i,c=0;
-            while(k<n && s.substr(k,nw)==w){
-                k+=nw;
+//     remember concatenate means in chain not random places
+    int maxRepeating(string sequence, string word) {
+        int n = sequence.size();
+        int m = word.size();
+        int k = 0;
+        if(m>n)
+            return 0;
+        else if(n==m && sequence==word)
+            return 1;
+        for(int i=0; i<n; i++){
+            int j=i; int c=0;
+            while(j<n && sequence.substr(j,m)==word){
+                j+=m;
                 c++;
             }
-            mx=max(mx,c);
+            k = max(k,c);
         }
-        return mx;
+        
+        return k;
     }
 };
+// class Solution {
+// public:
+//     int maxRepeating(string s, string w) {
+//         int mx=0,nw=w.size(),n=s.size();
+//         if(nw>n) return 0;
+//         for(int i=0;i<n;i++){
+//             int k=i,c=0;
+//             while(k<n && s.substr(k,nw)==w){
+//                 k+=nw;
+//                 c++;
+//             }
+//             mx=max(mx,c);
+//         }
+//         return mx;
+//     }
+// };
