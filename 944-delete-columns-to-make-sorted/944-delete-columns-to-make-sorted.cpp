91f8d0cf1827b1a710ccad1aa8018b2f225d19d1
Parija Annuprit <71126167+ParijaAnnuprit@users.noreply.github.com>
2022-06-09 23:34:47 +0530

    Time: 46 ms (84.97%), Space: 12.2 MB (27.73%) - LeetHub

--- 944-delete-columns-to-make-sorted/944-delete-columns-to-make-sorted.cpp
+++ 944-delete-columns-to-make-sorted/944-delete-columns-to-make-sorted.cpp
@@ -1,34 +1,34 @@
-// class Solution {
-// public:
-//     int minDeletionSize(vector<string>& strs) {
-//         int delcol = 0;
-//         int n = strs.size();
-//         int m = strs[0].size();
-//         for(int c=0;c<m;c++){
-//             for(int i=0;i<n-1;i++){
-//                 if(int(strs[i][c]) > int(strs[i+1][c]) )
-//                     delcol++;
-//                     break;
-//             }
-//         }
-//         return delcol;
-//     }
-// };
 class Solution {
 public:
     int minDeletionSize(vector<string>& strs) {
-        int ans=0;
-        for(int j=0;j<strs[0].size();j++)
-        {
-            for(int i=0;i<strs.size()-1;i++)
-            {
-                if(strs[i][j]>strs[i+1][j])
-                {
-                    ans++;
-                    break;
-                }
+        int delcol = 0;
+        int n = strs.size();
+        int m = strs[0].size();
+        for(int c=0;c<m;c++){
+            for(int i=0;i<n-1;i++){
+                if(strs[i][c] > strs[i+1][c] ){
+                    delcol++;
+                    break;}
             }
         }
-        return ans;
+        return delcol;
     }
 };
+// class Solution {
+// public:
+//     int minDeletionSize(vector<string>& strs) {
+//         int ans=0;
+//         for(int j=0;j<strs[0].size();j++)
+//         {
+//             for(int i=0;i<strs.size()-1;i++)
+//             {
+//                 if(strs[i][j]>strs[i+1][j])
+//                 {
+//                     ans++;
+//                     break;
+//                 }
+//             }
+//         }
+//         return ans;
+//     }
+// };
