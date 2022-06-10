aa64aaa60cfaf0f7a6ec7bae135f1cf982db4f34
Parija Annuprit <71126167+ParijaAnnuprit@users.noreply.github.com>
2022-06-10 21:08:44 +0530

    Time: 0 ms (100.00%), Space: 6 MB (45.74%) - LeetHub

--- 168-excel-sheet-column-title/168-excel-sheet-column-title.cpp
+++ 168-excel-sheet-column-title/168-excel-sheet-column-title.cpp
@@ -16,30 +16,29 @@
 //         return res;
 //     }
 // };
-// class Solution {
-// public:
-//     string convertToTitle(int columnNumber) {
-//         string res = "";
-//         int x = columnNumber;
-//         while(x){
-//             int rem = (--x)%26;
-//             res+= rem+ "A";
-//             x = x/26;
-//         }
-//         reverse(res.begin(),res.end());
-//         return res;
-//     }
-// };
 class Solution {
 public:
     string convertToTitle(int c) {
-        string appnd="";
-        while(c) {
-            int rem=(--c)%26;
-            appnd+=rem+'A';
-            c/=26;
+        string res = "";
+        while(c){
+            int rem = (--c)%26;
+            res+= rem+ 'A';
+            c = c/26;
         }
-        reverse(appnd.begin(), appnd.end());
-        return appnd;
+        reverse(res.begin(),res.end());
+        return res;
     }
 };
+// class Solution {
+// public:
+//     string convertToTitle(int c) {
+//         string appnd="";
+//         while(c) {
+//             int rem=(--c)%26;
+//             appnd+=rem+'A';
+//             c/=26;
+//         }
+//         reverse(appnd.begin(), appnd.end());
+//         return appnd;
+//     }
+// };
