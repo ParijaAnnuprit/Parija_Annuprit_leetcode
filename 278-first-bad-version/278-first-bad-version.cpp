ba1aa164505aa276cb9df6239da375106b664beb
Parija Annuprit <71126167+ParijaAnnuprit@users.noreply.github.com>
2022-06-11 17:16:31 +0530

    Time: 0 ms (100.00%), Space: 5.7 MB (99.26%) - LeetHub

--- 278-first-bad-version/278-first-bad-version.cpp
+++ 278-first-bad-version/278-first-bad-version.cpp
@@ -1,20 +1,35 @@
-// The API isBadVersion is defined for you.
-// bool isBadVersion(int version);
+// // The API isBadVersion is defined for you.
+// // bool isBadVersion(int version);
 
+// class Solution {
+// public:
+//     int firstBadVersion(int n) {
+//         int l = 1;
+//         int h = n;
+//         while(l<=h){
+//             int m = l+ ((h-l)/2);
+//             if(isBadVersion(m))
+//                 if(m==1 || isBadVersion(m-1)== false) return m;
+//                 else
+//                     h = m-1;
+//             else
+//                 l = m+1;
+//         }
+//         return -1;
+//     }
+// };
 class Solution {
 public:
     int firstBadVersion(int n) {
         int l = 1;
         int h = n;
-        while(l<=h){
-            int m = l+ ((h-l)/2);
+        while(l<h){
+            int m = l+((h-l)/2);
             if(isBadVersion(m))
-                if(m==1 || isBadVersion(m-1)== false) return m;
-                else
-                    h = m-1;
+                h = m;
             else
                 l = m+1;
         }
-        return -1;
+        return l;
     }
 };
