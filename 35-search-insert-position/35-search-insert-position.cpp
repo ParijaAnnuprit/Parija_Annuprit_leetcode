d6bd97d10b929eb0005aa1a524f4ed5e041ac840
Parija Annuprit <71126167+ParijaAnnuprit@users.noreply.github.com>
2022-06-12 15:39:17 +0530

    Time: 0 ms (100.00%), Space: 9.6 MB (94.48%) - LeetHub

--- 35-search-insert-position/35-search-insert-position.cpp
+++ 35-search-insert-position/35-search-insert-position.cpp
@@ -1,20 +1,37 @@
+// class Solution {
+// public:
+//     int searchInsert(vector<int>& nums, int target) {
+//         int n = nums.size();
+//         int l = 0;
+//         int h = n-1;
+//         int ans = 0;
+//         while(l<=h){
+//             int m = l+ ((h-l)/2);
+//             if(nums[m] == target)
+//                 return m;
+//             else if(nums[m] > target){
+//                 h = m-1;
+//                 }
+//             else if(nums[m] < target){
+//                 l = m+1;
+//                 }
+//         }
+//         return l;
+//     }
+// };
 class Solution {
 public:
     int searchInsert(vector<int>& nums, int target) {
         int n = nums.size();
         int l = 0;
-        int h = n-1;
-        int ans = 0;
-        while(l<=h){
-            int m = l+ ((h-l)/2);
-            if(nums[m] == target)
-                return m;
-            else if(nums[m] > target){
-                h = m-1;
-                }
-            else if(nums[m] < target){
+        int h = n;
+        while(l<h){
+            int m = l+((h-l)/2);
+            if(nums[m]>=target)
+                h = m;
+            else
                 l = m+1;
-                }
+                
         }
         return l;
     }
