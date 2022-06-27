fd17b6ee5fc0501ed9e25c4586f60237eb8fa787
Parija Annuprit <71126167+ParijaAnnuprit@users.noreply.github.com>
2022-06-27 17:31:30 +0530

    Time: 0 ms (100.00%), Space: 6.5 MB (88.86%) - LeetHub

--- 58-length-of-last-word/58-length-of-last-word.cpp
+++ 58-length-of-last-word/58-length-of-last-word.cpp
@@ -1,21 +1,35 @@
+// class Solution {
+// public:
+//     int lengthOfLastWord(string s) {
+//         int n = s.length();
+//         int j=n-1;
+//         int k=n-1;
+//         for(int i=n-1;i>=0;i--){
+//             if(isalpha(s[i])){
+//                 j=i;
+//                 break;
+//             }
+//         }
+//         for(int i=n-1;i>=0;i--){
+//             if(i==0 || (isalpha(s[i]) && s[i-1]==' ')){
+//                 k=i;
+//                 break;
+//             }
+//         }
+//         return (j-k+1);
+//     }
+// };
 class Solution {
 public:
     int lengthOfLastWord(string s) {
-        int n = s.length();
-        int j=n-1;
-        int k=n-1;
-        for(int i=n-1;i>=0;i--){
-            if(isalpha(s[i])){
-                j=i;
-                break;
+        int count=0;
+        for(int i=s.size()-1;i>=0;i--){
+            if(!isblank(s[i])){
+                count++;
+                if(i>0 && isblank(s[i-1]))
+                    break;        
             }
         }
-        for(int i=n-1;i>=0;i--){
-            if(i==0 || (isalpha(s[i]) && s[i-1]==' ')){
-                k=i;
-                break;
-            }
-        }
-        return (j-k+1);
+        return count;
     }
 };
