4ae5a49ba92440c8853e5d96ad4a2402876a073e
Parija Annuprit <71126167+ParijaAnnuprit@users.noreply.github.com>
2022-07-07 19:00:32 +0530

    Time: 100 ms (5.95%), Space: 25.8 MB (93.20%) - LeetHub

---
+++ 495-teemo-attacking/495-teemo-attacking.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    int findPoisonedDuration(vector<int>& timeSeries, int duration) {
+        int ans=0;
+        for(int i=0;i<timeSeries.size()-1;i++){
+            if(timeSeries[i+1]<=timeSeries[i]+duration){
+                ans+=timeSeries[i+1]-timeSeries[i];
+            }
+            else
+                ans+=duration;
+        }
+        return ans+=duration;
+    }
+};
+
+
+
+
+
+
+
+
+
+// class Solution {
+// public:
+//     int findPoisonedDuration(vector<int>& timeSeries, int duration) {
+//         int ans=0;
+//         for(int i=0; i<timeSeries.size()-1; i++){
+//             if(timeSeries[i+1]<= timeSeries[i]+duration){
+//                 ans+=timeSeries[i+1]-timeSeries[i];
+//             }
+//             else{
+//                 ans+=duration;
+//             }
+//         }
+//         ans+=duration;
+//         return ans;
+//     }
+// };
