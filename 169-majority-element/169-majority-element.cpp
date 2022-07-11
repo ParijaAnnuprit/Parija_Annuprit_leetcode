44db2d56f850b423e458223525570b0f5569dee0
Parija Annuprit <71126167+ParijaAnnuprit@users.noreply.github.com>
2022-07-11 19:29:43 +0530

    Time: 45 ms (11.39%), Space: 19.6 MB (58.16%) - LeetHub

--- 169-majority-element/169-majority-element.cpp
+++ 169-majority-element/169-majority-element.cpp
@@ -1,24 +1,45 @@
+// class Solution {
+// public:
+//     int majorityElement(vector<int>& nums) {
+//         int n = nums.size();
+//         sort(nums.begin(),nums.end());
+//         int count = 1;
+//         if(n==1 || n==2)
+//             return nums[0];
+//         for(int i=0;i<n;i++){
+//             if(nums[i] == nums[i+1])
+//                 count++;
+//             else if(nums[i]!= nums[i+1]){
+//                 if(count>(n/2))
+//                     return nums[i];
+//                 else{
+//                     count=1;
+//                 }
+                    
+//             }
+//         }
+//         return -1;
+//         // return count;
+//     }
+// };
+
+
+
+
 class Solution {
 public:
     int majorityElement(vector<int>& nums) {
-        int n = nums.size();
-        sort(nums.begin(),nums.end());
-        int count = 1;
-        if(n==1 || n==2)
-            return nums[0];
-        for(int i=0;i<n;i++){
-            if(nums[i] == nums[i+1])
-                count++;
-            else if(nums[i]!= nums[i+1]){
-                if(count>(n/2))
+        unordered_map<int,int>mpp;
+        for(int i=0;i<nums.size();i++){
+            if(mpp.find(nums[i])!=mpp.end()){
+                mpp[nums[i]]++;
+                if(mpp[nums[i]]>(nums.size()/2))
                     return nums[i];
-                else{
-                    count=1;
-                }
-                    
+            }
+            else{
+                mpp[nums[i]]=1;
             }
         }
-        return -1;
-        // return count;
+        return 1;
     }
 };
