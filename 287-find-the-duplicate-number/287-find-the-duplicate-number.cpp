2f6052f3725a2b39eb3329d3e9c0e1a7d0b4c70a
Parija Annuprit <71126167+ParijaAnnuprit@users.noreply.github.com>
2022-07-23 19:25:51 +0530

    Time: 330 ms (12.42%), Space: 84 MB (15.72%) - LeetHub

--- 287-find-the-duplicate-number/287-find-the-duplicate-number.cpp
+++ 287-find-the-duplicate-number/287-find-the-duplicate-number.cpp
@@ -11,3 +11,23 @@ public:
         return 0;
     }
 };
+
+// class Solution {
+// public:
+//     int findDuplicate(vector<int>& nums) {
+//         int n = nums.size()-1;
+//         for(int i=1;i<n;i++){
+//             for(int j=0;j<nums.size();j++){
+//                 if(i==nums[j]){
+//                     nums[j]= nums[j]^i;
+//                     break;
+//                 }
+//             }
+//         }
+//         for(int i=0;i<nums.size();i++){
+//             if(nums[i]!=0)
+//                 return nums[i];
+//         }
+//         return 0;
+//     }
+// };
