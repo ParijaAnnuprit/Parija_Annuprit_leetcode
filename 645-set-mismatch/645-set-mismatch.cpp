cef1ba3d753773fb24a7f571b8437d80116ef410
Parija Annuprit <71126167+ParijaAnnuprit@users.noreply.github.com>
2022-07-23 16:54:02 +0530

    Time: 114 ms (20.20%), Space: 30.8 MB (26.41%) - LeetHub

--- 645-set-mismatch/645-set-mismatch.cpp
+++ 645-set-mismatch/645-set-mismatch.cpp
@@ -1,23 +1,45 @@
+// class Solution {
+// public:
+//     vector<int> findErrorNums(vector<int>& arr) {
+//         int i =0;
+//          int n = arr.size();
+//         vector<int> ans;
+//          while(i<n){
+//              int c_var = arr[i]-1;
+//              if(arr[i]!=arr[c_var]){
+//                 swap(arr[i],arr[c_var]);
+//              }else{
+//                 i++;
+//              }
+//           }
+//         for(int j=0;j<n;j++){
+//             if(j+1!=arr[j]){
+//                 ans.push_back(arr[j]);
+//                 ans.push_back(j+1);
+//             }
+//         }
+//         return ans;
+//     }
+// };
 class Solution {
 public:
     vector<int> findErrorNums(vector<int>& arr) {
-        int i =0;
-         int n = arr.size();
-        vector<int> ans;
-         while(i<n){
-             int c_var = arr[i]-1;
-             if(arr[i]!=arr[c_var]){
-                swap(arr[i],arr[c_var]);
-             }else{
-                i++;
-             }
-          }
-        for(int j=0;j<n;j++){
-            if(j+1!=arr[j]){
-                ans.push_back(arr[j]);
-                ans.push_back(j+1);
-            }
+        unordered_map<int,int>mpp;
+        vector<int>res;
+        for(int i=1;i<=arr.size();i++){
+            mpp[i]=0;
         }
-        return ans;
+        for(int i=0;i<arr.size();i++){
+            mpp[arr[i]]++;
+        }
+        for(auto s:mpp){
+            if(s.second==2)
+                res.push_back(s.first);
+        }
+        for(auto s:mpp){
+            if(s.second==0)
+                res.push_back(s.first);
+        }
+        return res;
     }
 };
