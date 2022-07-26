9285fc92589c662818b88c22bf5a67fbbd78eac9
Parija Annuprit <71126167+ParijaAnnuprit@users.noreply.github.com>
2022-07-26 16:44:51 +0530

    Time: 12 ms (59.34%), Space: 11.3 MB (58.11%) - LeetHub

---
+++ 1636-sort-array-by-increasing-frequency/1636-sort-array-by-increasing-frequency.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    vector<int> frequencySort(vector<int>& nums) {
+        map<int,int>mpp;
+        for(int i=0;i<nums.size();i++){
+            mpp[nums[i]]++;
+        }
+        auto cmp= [&](int& a, int& b ){
+            return mpp[a]==mpp[b]? a>b: mpp[a]<mpp[b];
+        };
+        sort(nums.begin(),nums.end(),cmp);
+        return nums;
+    }
+};
+
+
+
+
+
+
+
+
+// class Solution {
+// public:
+//     vector<int> frequencySort(vector<int>& nums) {
+//         map<int,int> seen;
+//         for(int i=0;i<nums.size();i++)
+//         {
+//             seen[nums[i]]++;
+//         }
+//         auto cmp=[&](const int& a, const int& b)
+//         {
+//             return seen[a]==seen[b]? a>b: seen[a]<seen[b];
+//         };//cmp
+//         sort(nums.begin(),nums.end(),cmp);
+//         return nums;
+//     }
+// };
