1efb9d5110b7e15f6333c1cbd27e39c29168d7a8
Parija Annuprit <71126167+ParijaAnnuprit@users.noreply.github.com>
2022-07-29 16:57:42 +0530

    Time: 19 ms (17.14%), Space: 14.9 MB (42.25%) - LeetHub

--- 21-merge-two-sorted-lists/21-merge-two-sorted-lists.cpp
+++ 21-merge-two-sorted-lists/21-merge-two-sorted-lists.cpp
@@ -8,21 +8,52 @@
  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
  * };
  */
+// class Solution {
+// public:
+//     ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
+//         if(list1==NULL)
+//             return list2;
+//         else if(list2 == NULL)
+//             return list1;
+//         else if(list1->val <= list2->val){
+//             list1->next = mergeTwoLists(list1->next, list2);
+//             return list1;
+//         }    
+//         else{
+//             list2->next = mergeTwoLists(list1, list2->next);
+//             return list2;
+//         }
+//         return NULL;
+//     }
+// };
+
 class Solution {
 public:
     ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
-        if(list1==NULL)
-            return list2;
-        else if(list2 == NULL)
-            return list1;
-        else if(list1->val <= list2->val){
-            list1->next = mergeTwoLists(list1->next, list2);
-            return list1;
-        }    
-        else{
-            list2->next = mergeTwoLists(list1, list2->next);
-            return list2;
+        ListNode* dummy = new ListNode(0);
+        // ListNode* temp1 = list1;
+        // ListNode* temp2 = list2;
+        ListNode* tempf = dummy;
+    
+        while(list1!=NULL && list2!= NULL){
+            if(list1->val<= list2->val){
+                dummy->next = list1;
+                dummy = dummy->next;
+                list1= list1->next;
+            }
+            else if(list2->val< list1->val){
+                dummy->next = list2;
+                dummy = dummy->next;
+                list2= list2->next;
+            }
+            
         }
-        return NULL;
+        if(list1== NULL){
+            dummy->next = list2;;
+        }
+        else if(list2== NULL){
+            dummy->next = list1;
+        }
+        return tempf->next;
     }
 };
