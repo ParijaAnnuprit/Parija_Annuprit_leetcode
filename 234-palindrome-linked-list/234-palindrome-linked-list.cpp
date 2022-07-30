f1982f55895af9204af42a6d1e35928d0e1cd6ff
Parija Annuprit <71126167+ParijaAnnuprit@users.noreply.github.com>
2022-07-30 22:29:47 +0530

    Time: 331 ms (62.79%), Space: 116.4 MB (65.81%) - LeetHub

---
+++ 234-palindrome-linked-list/234-palindrome-linked-list.cpp
@@ -0,0 +1,62 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+
+ListNode* reverse(ListNode* head){
+    ListNode* newhead = NULL;
+    if(head==NULL || head->next == NULL){
+        return head;
+    }
+    else{
+        newhead = reverse(head->next);
+        head->next->next = head;
+        head->next = NULL;
+    }
+    return newhead;
+}
+
+class Solution {
+public:
+    bool isPalindrome(ListNode* head) {
+        ListNode* slow = head;
+        ListNode* fast = head;
+        ListNode* revhead = NULL;
+        if(head->next == NULL)
+            return true;
+        else if(head->next->next == NULL){
+            if(head->val == head->next->val)
+                return true;
+            else
+                return false;
+        }
+        while(fast!=NULL && fast->next != NULL){
+            fast = fast->next->next;
+//           odd case
+            if(fast->next == NULL){
+                revhead = reverse(slow->next->next);
+                break;
+            }
+//             even case
+            else if(fast->next->next == NULL){
+                slow = slow->next;
+                revhead = reverse(slow->next);
+                break;
+            }
+            slow = slow->next;
+        }
+        while(revhead!=NULL){
+            if(revhead->val != head->val)
+                return false;
+            revhead = revhead->next;
+            head = head->next;
+        }
+        return true;
+    }
+};
