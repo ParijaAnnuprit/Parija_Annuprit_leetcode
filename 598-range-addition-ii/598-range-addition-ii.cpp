67427fe417b59f52d58ccdd00c6278fcf74d3fe3
Parija Annuprit <71126167+ParijaAnnuprit@users.noreply.github.com>
2022-08-01 16:04:56 +0530

    Time: 22 ms (27.47%), Space: 10.9 MB (84.02%) - LeetHub

---
+++ 598-range-addition-ii/598-range-addition-ii.cpp
@@ -0,0 +1,37 @@
+// class Solution {
+// public:
+//     int maxCount(int m, int n, vector<vector<int>>& ops) {
+//         vector<vector<int>>v(m,vector<int>(n,0));
+//         for(int i=0;i<ops.size();i++){
+//             for(int j=0;j<ops[i][0];j++){
+//                 for(int k=0;k<ops[i][1];k++){
+//                     v[j][k]++;
+//                 }
+//             }
+//         }
+//         int count=0;
+//         int x = v[0][0];
+//         for(int i=0;i<m;i++){
+//             for(int j=0;j<n;j++){
+//                 if(v[i][j]==x)
+//                     count++;
+//             }
+//         }
+//         return count;
+//     }
+// };
+
+class Solution {
+public:
+    int maxCount(int m, int n, vector<vector<int>>& ops) {
+        if(ops.size()==0)
+            return m*n;
+        int minr = INT_MAX;
+        int minc = INT_MAX;
+        for(int i=0;i<ops.size();i++){
+            minr = min(minr,ops[i][0]);
+            minc = min(minc,ops[i][1]);
+        }
+        return minr*minc; 
+    }
+};
