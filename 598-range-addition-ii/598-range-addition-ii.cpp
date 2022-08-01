66e1d9f5df18b6682bd85a91c7d4f779a5a25017
Parija Annuprit <71126167+ParijaAnnuprit@users.noreply.github.com>
2022-08-01 16:09:04 +0530

    Time: 33 ms (5.75%), Space: 11.5 MB (22.62%) - LeetHub

--- 598-range-addition-ii/598-range-addition-ii.cpp
+++ 598-range-addition-ii/598-range-addition-ii.cpp
@@ -1,6 +1,7 @@
 // class Solution {
 // public:
 //     int maxCount(int m, int n, vector<vector<int>>& ops) {
+// o(n^3) memory limit exceeded
 //         vector<vector<int>>v(m,vector<int>(n,0));
 //         for(int i=0;i<ops.size();i++){
 //             for(int j=0;j<ops[i][0];j++){
@@ -21,17 +22,30 @@
 //     }
 // };
 
+// class Solution {
+// public:
+//     int maxCount(int m, int n, vector<vector<int>>& ops) {
+// //         tc = o(n) sc = o(1)
+//         if(ops.size()==0)
+//             return m*n;
+//         int minr = INT_MAX;
+//         int minc = INT_MAX;
+//         for(int i=0;i<ops.size();i++){
+//             minr = min(minr,ops[i][0]);
+//             minc = min(minc,ops[i][1]);
+//         }
+//         return minr*minc; 
+//     }
+// };
+
 class Solution {
 public:
     int maxCount(int m, int n, vector<vector<int>>& ops) {
-        if(ops.size()==0)
-            return m*n;
-        int minr = INT_MAX;
-        int minc = INT_MAX;
-        for(int i=0;i<ops.size();i++){
-            minr = min(minr,ops[i][0]);
-            minc = min(minc,ops[i][1]);
+//         tc = o(n) sc = o(1)
+        for(auto i:ops){
+            m = min(m,i[0]);
+            n = min(n,i[1]);
         }
-        return minr*minc; 
+        return m*n;
     }
 };
