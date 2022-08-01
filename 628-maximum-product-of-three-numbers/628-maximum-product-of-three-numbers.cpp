faa191218171e4d4e85b21fa5f8488881849f7cf
Parija Annuprit <71126167+ParijaAnnuprit@users.noreply.github.com>
2022-08-01 14:37:47 +0530

    Time: 101 ms (7.86%), Space: 27.7 MB (36.22%) - LeetHub

--- 628-maximum-product-of-three-numbers/628-maximum-product-of-three-numbers.cpp
+++ 628-maximum-product-of-three-numbers/628-maximum-product-of-three-numbers.cpp
@@ -4,20 +4,9 @@ public:
         int n = nums.size();
         if(n==3)
             return nums[0]*nums[1]*nums[2];
-        int neg = 0;
-        
         sort(nums.begin(),nums.end());
-        // for(int i=0;i<n;i++){
-        //     if(nums[i]>0)
-        //         break;
-        //     else if(nums[i]<0){
-        //         neg++;
-        //     }
-        // }
-        // if(neg==0 || neg==1)
-        //     return nums[n-1]*nums[n-2]*nums[n-3];
-            if(nums[0]*nums[1]*nums[n-1]> nums[n-1]*nums[n-2]*nums[n-3])
-                return nums[0]*nums[1]*nums[n-1];
+        if(nums[0]*nums[1]*nums[n-1]> nums[n-1]*nums[n-2]*nums[n-3])
+            return nums[0]*nums[1]*nums[n-1];
         return nums[n-1]*nums[n-2]*nums[n-3];
     }
 };
