b64f9350056c9c47893cf8e9d07d768615d01464
Parija Annuprit <71126167+ParijaAnnuprit@users.noreply.github.com>
2022-08-02 22:18:52 +0530

    Time: 404 ms (49.11%), Space: 144.8 MB (43.01%) - LeetHub

--- 111-minimum-depth-of-binary-tree/111-minimum-depth-of-binary-tree.cpp
+++ 111-minimum-depth-of-binary-tree/111-minimum-depth-of-binary-tree.cpp
@@ -9,41 +9,17 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
-// class Solution {
-// public:
-//     int minDepth(TreeNode* root) {
-//       if(root==NULL)
-//           return 0;
-//        int x = 1+minDepth(root->left);
-//        int y = 1+minDepth(root->right);
-//         if(x==1)
-//             return y;
-//         else if(y==1)
-//             return x;
-//         return min(x,y);
-//     }
-// };
-
 class Solution {
 public:
-    void maxlevel(TreeNode* root,int level,int &ans){
-        if(!root)
-            return ;
-        
-        if(!root->left && !root->right){
-            ans=min(level,ans);
-            return ;
-        }
-        
-        maxlevel(root->left,level+1,ans);
-        maxlevel(root->right,level+1,ans);
-    }
     int minDepth(TreeNode* root) {
-        if(!root)
-            return 0;
-        
-        int ans=INT_MAX;
-        maxlevel(root,0,ans);
-        return ans+1;
+      if(root==NULL)
+          return 0;
+       int x = 1+minDepth(root->left);
+       int y = 1+minDepth(root->right);
+        if(x==1)
+            return y;
+        else if(y==1)
+            return x;
+        return min(x,y);
     }
 };
