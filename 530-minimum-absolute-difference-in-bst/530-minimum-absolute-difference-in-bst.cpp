30fb4f72618c0f9b67efe760e5b8446a5af3038b
Parija Annuprit <71126167+ParijaAnnuprit@users.noreply.github.com>
2022-08-03 19:42:12 +0530

    Time: 31 ms (47.00%), Space: 25 MB (96.30%) - LeetHub

--- 530-minimum-absolute-difference-in-bst/530-minimum-absolute-difference-in-bst.cpp
+++ 530-minimum-absolute-difference-in-bst/530-minimum-absolute-difference-in-bst.cpp
@@ -9,29 +9,6 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
-class Solution {
-public:
-    vector<int>v;
-    void inorder(TreeNode* root){
-        if(root==NULL)
-            return;
-        inorder(root->left);
-        v.push_back(root->val);
-        inorder(root->right);
-    }
-    int getMinimumDifference(TreeNode* root) {
-        TreeNode* curr = root;
-        // vector<int>res;
-        inorder(curr);
-        int diff = INT_MAX;
-        sort(v.begin(),v.end());
-        for(int i=1;i<v.size();i++)
-            diff = min(diff, v[i]-v[i-1]);
-        // sort(res.begin(),res.end());
-        return diff;
-    }
-};
-
 // class Solution {
 // public:
 //     vector<int>v;
@@ -42,14 +19,35 @@ public:
 //         v.push_back(root->val);
 //         inorder(root->right);
 //     }
-//     int minDiffInBST(TreeNode* root) {
+//     int getMinimumDifference(TreeNode* root) {
 //         TreeNode* curr = root;
-//         vector<int>res;
+//         // vector<int>res;
 //         inorder(curr);
+//         int diff = INT_MAX;
 //         sort(v.begin(),v.end());
 //         for(int i=1;i<v.size();i++)
-//             res.push_back(v[i]-v[i-1]);
-//         sort(res.begin(),res.end());
-//         return res[0];
+//             diff = min(diff, v[i]-v[i-1]);
+//         // sort(res.begin(),res.end());
+//         return diff;
 //     }
 // };
+
+class Solution {
+    TreeNode* pre = nullptr;
+    int minimum = std::numeric_limits<int>:: max();
+public:
+    void inorder(TreeNode* node) {
+        if (!node) return;
+        inorder(node->left);
+        if(pre) {
+            minimum = min(node->val - pre->val, minimum);
+        }
+        pre = node;
+        inorder(node->right);
+    }
+    int getMinimumDifference(TreeNode* root) {
+        inorder(root);
+        return minimum;
+    }
+
+};
