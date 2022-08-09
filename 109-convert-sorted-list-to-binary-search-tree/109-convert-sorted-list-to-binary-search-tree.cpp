179505c60bdc90a6e52e22182379585f0aebfe3b
Parija Annuprit <71126167+ParijaAnnuprit@users.noreply.github.com>
2022-08-09 18:43:27 +0530

    Time: 578 ms (5.02%), Space: 363 MB (5.18%) - LeetHub

--- 109-convert-sorted-list-to-binary-search-tree/109-convert-sorted-list-to-binary-search-tree.cpp
+++ 109-convert-sorted-list-to-binary-search-tree/109-convert-sorted-list-to-binary-search-tree.cpp
@@ -19,52 +19,55 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
-class Solution {
-public:
-    TreeNode* sortedListToBST(ListNode* head) {
-        return helper(head);
-    }
-    TreeNode* helper(ListNode* head){
-       if(head==NULL) return NULL;
-        ListNode *dummy=NULL;
-        ListNode* slow = head;
-         
-        ListNode* fast = head;
-        // dummy->next = head;
-        while(fast!=NULL && fast->next!=NULL){
-            dummy=slow;
-            slow = slow->next;
-            fast = fast->next->next;
-            
-        }
-         // if(head==slow) return nullptr;
-    
-        TreeNode* root = new TreeNode(slow->val);
-        if(dummy){
-            dummy->next=NULL;
-            root->left = helper(head);
-        }
-        
-      root->right = helper(slow->next);
-        return root;
-    }
-};
-
-
 // class Solution {
 // public:
 //     TreeNode* sortedListToBST(ListNode* head) {
-//         vector<int>v;
-//         while(head != NULL){
-//             v.push_back(head->val);
-//             head= head->next;
-//         }
-//         return helper(v, 0, v.size()-1);    
+//         return helper(head);
 //     }
-//     TreeNode* helper(vector<int>v, int b, int e){
-//         int mid = (b+e)/2;
-//         TreeNode
-//         if(b>e) return nullptr;
+//     TreeNode* helper(ListNode* head){
+//        if(head==NULL) return NULL;
+//         ListNode *dummy=NULL;
+//         ListNode* slow = head;
+         
+//         ListNode* fast = head;
+//         // dummy->next = head;
+//         while(fast!=NULL && fast->next!=NULL){
+//             dummy=slow;
+//             slow = slow->next;
+//             fast = fast->next->next;
+            
+//         }
+//          // if(head==slow) return nullptr;
+    
+//         TreeNode* root = new TreeNode(slow->val);
+//         if(dummy){
+//             dummy->next=NULL;
+//             root->left = helper(head);
+//         }
         
+//       root->right = helper(slow->next);
+//         return root;
 //     }
 // };
+
+
+class Solution {
+public:
+    TreeNode* sortedListToBST(ListNode* head) {
+        if(head == NULL ) return NULL;
+        vector<int>v;
+        while(head != NULL){
+            v.push_back(head->val);
+            head= head->next;
+        }
+        return helper(v, 0, v.size()-1);    
+    }
+    TreeNode* helper(vector<int>v, int b, int e){
+        int mid = (b+e)/2;
+        TreeNode* root = new TreeNode(v[mid]);
+        if(b>e) return nullptr;
+        root->left = helper(v, b, mid-1);
+        root->right = helper(v, mid+1, e);
+        return root;
+    }
+};
