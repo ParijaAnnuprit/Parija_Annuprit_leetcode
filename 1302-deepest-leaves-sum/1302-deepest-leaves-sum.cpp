46a52c5c867969d1b0c448785b77ec713d37fddc
Parija Annuprit <71126167+ParijaAnnuprit@users.noreply.github.com>
2022-08-11 13:14:05 +0530

    Time: 128 ms (77.20%), Space: 61.8 MB (50.13%) - LeetHub

--- 1302-deepest-leaves-sum/1302-deepest-leaves-sum.cpp
+++ 1302-deepest-leaves-sum/1302-deepest-leaves-sum.cpp
@@ -9,27 +9,56 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
+// class Solution {
+// public:
+//     int deepestLeavesSum(TreeNode* root) {
+//         queue<TreeNode*>q;
+//         q.push(root);
+//         vector<int>level;
+//         while(!q.empty()){
+//             int x = q.size();
+//             level.clear();
+//             for(int i=0;i<x;i++){
+//                 TreeNode* temp = q.front();
+//                 q.pop();
+//                 if(temp->left!=NULL) q.push(temp->left);
+//                 if(temp->right!=NULL) q.push(temp->right);
+//                 level.push_back(temp->val);
+//             }
+//         }
+//         int sum = 0;
+//         for(int i=0;i<level.size();i++){
+//             sum += level[i];
+//         }
+//         return sum;
+//     }
+// };
+
 class Solution {
 public:
     int deepestLeavesSum(TreeNode* root) {
+        int k = maxheight(root);
         queue<TreeNode*>q;
         q.push(root);
-        vector<int>level;
+        int level = 0;
+        int sum =0;
         while(!q.empty()){
             int x = q.size();
-            level.clear();
+            level++;
             for(int i=0;i<x;i++){
                 TreeNode* temp = q.front();
                 q.pop();
+                if(level == k){
+                    sum+= temp->val;
+                }
                 if(temp->left!=NULL) q.push(temp->left);
                 if(temp->right!=NULL) q.push(temp->right);
-                level.push_back(temp->val);
             }
         }
-        int sum = 0;
-        for(int i=0;i<level.size();i++){
-            sum += level[i];
-        }
         return sum;
+    }
+    int maxheight(TreeNode* root){
+        if(root==NULL) return 0;
+        return max(maxheight(root->left)+1, maxheight(root->right)+1);
     }
 };
