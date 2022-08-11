fc41a09a26ad51410d09928e6b9e112fa5aea33f
Parija Annuprit <71126167+ParijaAnnuprit@users.noreply.github.com>
2022-08-11 16:15:57 +0530

    Time: 0 ms (100.00%), Space: 7.1 MB (25.98%) - LeetHub

--- 671-second-minimum-node-in-a-binary-tree/671-second-minimum-node-in-a-binary-tree.cpp
+++ 671-second-minimum-node-in-a-binary-tree/671-second-minimum-node-in-a-binary-tree.cpp
@@ -9,22 +9,45 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
+// class Solution {
+// public:
+//     int min1 = INT_MAX;
+//     int min2 = INT_MAX;
+//     // vector<int>v;
+//     int findSecondMinimumValue(TreeNode* root) {
+//         inorder(root);
+//         return min2<INT_MAX ? min2:-1;
+//     }
+//     void inorder(TreeNode* root){
+//         if(root == NULL) return;
+//         inorder(root->left);
+//         if(min1<root->val){
+//             min2 = min1;
+//             min1 = root->val;
+//         }
+//         if(root->val < min2 && root->val > min1) min2 = root->val;
+//         inorder(root->right);
+//     }
+// };
+
 class Solution {
 public:
-    vector<int>v;
-    int findSecondMinimumValue(TreeNode* root) {
-        inorder(root);
-        sort(v.begin(),v.end());
-        for(int i=1;i<v.size();i++){
-            if(v[i]>v[0])
-                return v[i];
-        }
-        return -1;
-    }
-    void inorder(TreeNode* root){
-        if(root == NULL) return;
-        inorder(root->left);
-        v.push_back(root->val);
-        inorder(root->right);
+    long long a = pow(2, 31), b = pow(2, 31);
+    
+    void findMin(TreeNode* root) {
+	if(!root) return;
+    findSecondMinimumValue(root->left);
+    if (root->val < a) {
+		b = a;
+		a = root->val;
+	}
+    if (root->val < b && root->val > a) b = root->val;
+    findSecondMinimumValue(root->right);
+}
+    
+int findSecondMinimumValue(TreeNode* root) {
+	findMin(root);
+    return b == pow(2, 31) ? -1 : b;
+        
     }
 };
