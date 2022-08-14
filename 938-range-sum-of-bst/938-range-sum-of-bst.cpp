20541574edabd2e0b0050312010569cfff190825
Parija Annuprit <71126167+ParijaAnnuprit@users.noreply.github.com>
2022-08-15 02:08:12 +0530

    Time: 160 ms (75.32%), Space: 64.7 MB (61.65%) - LeetHub

--- 938-range-sum-of-bst/938-range-sum-of-bst.cpp
+++ 938-range-sum-of-bst/938-range-sum-of-bst.cpp
@@ -9,18 +9,31 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
+// class Solution {
+// public:
+//     // vector<int>v;
+//     int sum =0;
+//     int rangeSumBST(TreeNode* root, int low, int high) {
+//         inorder(root,low,high);
+//         return sum;
+//     }
+//     void inorder(TreeNode* root , int low , int high){
+//         if(root == NULL) return;
+//         inorder(root->left, low, high);
+//         if(root->val>=low && root->val<=high) sum+= root->val;
+//         inorder(root->right, low , high);
+//     }
+// };
+
 class Solution {
 public:
     // vector<int>v;
     int sum =0;
     int rangeSumBST(TreeNode* root, int low, int high) {
-        inorder(root,low,high);
+        if(root == NULL) return 0;
+        rangeSumBST(root->left, low,high);
+        if(root->val >= low && root->val <= high) sum+=root->val;
+        rangeSumBST(root->right,low,high);
         return sum;
-    }
-    void inorder(TreeNode* root , int low , int high){
-        if(root == NULL) return;
-        inorder(root->left, low, high);
-        if(root->val>=low && root->val<=high) sum+= root->val;
-        inorder(root->right, low , high);
     }
 };
