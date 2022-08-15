0c882ed6346a118fbabc469d83e7eee25f03c447
Parija Annuprit <71126167+ParijaAnnuprit@users.noreply.github.com>
2022-08-15 17:04:53 +0530

    Time: 21 ms (36.62%), Space: 20.2 MB (93.14%) - LeetHub

--- 543-diameter-of-binary-tree/543-diameter-of-binary-tree.cpp
+++ 543-diameter-of-binary-tree/543-diameter-of-binary-tree.cpp
@@ -9,49 +9,19 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
-// class Solution {
-// public:
-//     unordered_map<TreeNode*,int>mpp;
-//     int diameterOfBinaryTree(TreeNode* root) {
-//         mpp[root] = 0;
-//         int a = INT_MIN;
-//         int b = INT_MAX;
-//         queue<TreeNode*>q;
-//         q.push(root);
-//         while(!q.empty()){
-//             int h = q.size();
-//             for(int i=0;i<h;i++){
-//                 TreeNode* temp = q.front();
-//                 q.pop();
-//                 if(temp->left) {
-//                     mpp[temp->left] = mpp[temp]-1;
-//                     q.push(temp->left);
-//                 }
-//                 if(temp->right){
-//                     mpp[temp->right]= mpp[temp]+1;
-//                     q.push(temp->right);
-//                 }
-//                 a = max(a,mpp[temp]);
-//                 b = min(b,mpp[temp]);
-//             }
-//         }
-//         return a-b;
-//     }
-// };
-
 class Solution {
 public:
-    int ans = 0;
-    int longestPath(TreeNode *root){
-        if(!root)
-            return -1;
-        int l = 1 + longestPath(root->left);
-        int r = 1 + longestPath(root->right);
-        ans = max(ans,l+r);
-        return max(l,r);
-    }
+    int maxi = INT_MIN;
     int diameterOfBinaryTree(TreeNode* root) {
-        longestPath(root);
-        return ans;
+        int lh = height(root->left);
+        int rh = height(root->right);
+        maxi = max(maxi,lh+rh);
+        if(root->left) diameterOfBinaryTree(root->left);
+        if(root->right) diameterOfBinaryTree(root->right);
+        return maxi;
+    }
+    int height(TreeNode* root){
+        if(root==NULL) return 0;
+        return max(1+height(root->left),1+height(root->right));
     }
 };
