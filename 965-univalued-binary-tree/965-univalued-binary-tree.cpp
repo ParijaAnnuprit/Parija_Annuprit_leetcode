25d7918751318e9d533415f49184b245691f2e6d
Parija Annuprit <71126167+ParijaAnnuprit@users.noreply.github.com>
2022-08-19 13:06:52 +0530

    Time: 3 ms (74.25%), Space: 9.9 MB (45.12%) - LeetHub

--- 965-univalued-binary-tree/965-univalued-binary-tree.cpp
+++ 965-univalued-binary-tree/965-univalued-binary-tree.cpp
@@ -9,20 +9,33 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
+// class Solution {
+// public:
+//     int flag = 0;
+//     bool isUnivalTree(TreeNode* root) {
+//         // if(root== NULL ) return false;
+//         helper(root, root->val);
+//         if(flag == 1) return false;
+//         return true;
+//     }
+//     void helper(TreeNode* root, int x){
+//         if(root == NULL) return;
+//         if(root->val != x) flag = 1;
+//         helper(root->left,x);
+//         helper(root->right,x);
+//         // return true;
+//     }
+// };
+
 class Solution {
 public:
     int flag = 0;
     bool isUnivalTree(TreeNode* root) {
-        // if(root== NULL ) return false;
-        helper(root, root->val);
-        if(flag == 1) return false;
-        return true;
+        return helper(root,root->val);
     }
-    void helper(TreeNode* root, int x){
-        if(root == NULL) return;
-        if(root->val != x) flag = 1;
-        helper(root->left,x);
-        helper(root->right,x);
-        // return true;
+    bool helper(TreeNode* root, int x){
+        if(root == NULL) return true;
+        if(root->val != x) return false;
+        return helper(root->left ,x) && helper(root->right,x);
     }
 };
