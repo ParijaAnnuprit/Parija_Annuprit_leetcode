b7f8c26d3b62b9a7ca15b60b90c95131e686a16d
Parija Annuprit <71126167+ParijaAnnuprit@users.noreply.github.com>
2022-08-19 21:35:28 +0530

    Time: 5 ms (34.43%), Space: 9.7 MB (15.32%) - LeetHub

--- 129-sum-root-to-leaf-numbers/129-sum-root-to-leaf-numbers.cpp
+++ 129-sum-root-to-leaf-numbers/129-sum-root-to-leaf-numbers.cpp
@@ -9,20 +9,43 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
+// class Solution {
+// public:
+// //     without backtracking
+//     int sumNumbers(TreeNode* root) {
+//         if(root==NULL) return 0;
+//         return helper(root,0);
+//     }
+//     int helper(TreeNode* root, int currsum){
+//         if(root == NULL) return 0;
+//         currsum = currsum*10 + root->val;
+//         if(root->left == NULL && root->right == NULL) return currsum;
+//         int left = helper(root->left,currsum);
+//         int right = helper(root->right,currsum);
+//         return left+right;
+//     }
+// };
+
 class Solution {
 public:
-//     tc = o(nh)
-    vector<vector<int>>v;
+//     with backtracking
+    vector<int>v;
     int sumNumbers(TreeNode* root) {
-        if(root==NULL) return 0;
-        return helper(root,0);
+        if(root== NULL) return  0;
+        helper(root,0);
+        int sum = 0;
+        for(int i=0;i<v.size();i++){
+            sum = sum+ v[i];
+        }
+        return sum;
     }
-    int helper(TreeNode* root, int currsum){
-        if(root == NULL) return 0;
-        currsum = currsum*10 + root->val;
-        if(root->left == NULL && root->right == NULL) return currsum;
-        int left = helper(root->left,currsum);
-        int right = helper(root->right,currsum);
-        return left+right;
+    void helper(TreeNode* root, int currsum){
+        if(root== NULL) return;
+        currsum = currsum*10 +root->val;
+        if(root->left== NULL && root->right == NULL) v.push_back(currsum);
+        helper(root->left,currsum);
+        helper(root->right,currsum);
+        currsum = currsum-root->val;
+        currsum = currsum/10;
     }
 };
