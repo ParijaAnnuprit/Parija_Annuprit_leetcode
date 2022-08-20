23602fd1ec898c94add83208669d142b79de8aab
Parija Annuprit <71126167+ParijaAnnuprit@users.noreply.github.com>
2022-08-20 14:22:34 +0530

    Time: 7 ms (56.68%), Space: 13.6 MB (22.39%) - LeetHub

--- 257-binary-tree-paths/257-binary-tree-paths.cpp
+++ 257-binary-tree-paths/257-binary-tree-paths.cpp
@@ -9,70 +9,70 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
 //  */
-// class Solution {
-// public:
-//     vector<string>v;
-//     vector<string> binaryTreePaths(TreeNode* root) {
-//         if(root== NULL) return v;
-//         string s = to_string(root->val);
-//         if(root->left== NULL && root->right == NULL){
-//             v.push_back(s);
-//             return v;
-//         }
-//         paths(root->left,s);
-//         paths(root->right,s);
-//         return v;
-//     }
-//     void paths(TreeNode* root, string s){
-//         if(root == NULL) return; 
-//         s = s+ "->"+to_string(root->val);
-//         if(root->left == NULL && root->right == NULL) v.push_back(s);
-//         if(root->left) paths(root->left,s);
-//         if(root->right) paths(root->right,s);
-//     }
-// };
-
- // */
 class Solution {
 public:
-    void path(TreeNode* root , vector<string> &ans, string output ){
-		// base case
-		if( root == NULL ){
-			return;
-		}
-    
-    // if  root is not null push the data into string
-    output += to_string( root->val);
-    
-    // if any left or right node of root exist push ->
-    if( root->left || root->right ){
-        output += "->";
+    vector<string>v;
+    vector<string> binaryTreePaths(TreeNode* root) {
+        if(root== NULL) return v;
+        string s = to_string(root->val);
+        if(root->left== NULL && root->right == NULL){
+            v.push_back(s);
+            return v;
+        }
+        paths(root->left,s);
+        paths(root->right,s);
+        return v;
     }
-    
-    if( !root->left && !root->right ){
-        ans.push_back( output); // store ans;
+    void paths(TreeNode* root, string s){
+        if(root == NULL) return; 
+        s = s+ "->"+to_string(root->val);
+        if(root->left == NULL && root->right == NULL) v.push_back(s);
+        if(root->left) paths(root->left,s);
+        if(root->right) paths(root->right,s);
     }
-    
-    
-    //leftside
-    if(root->left)
-        path(root->left , ans, output);
-    
-    //rightside
-    if(root->right)
-        path(root->right , ans, output);
-    
-    output.pop_back();  // backtrack
-    
-}
-
-vector<string> binaryTreePaths(TreeNode* root) {
-    vector<string> ans;  // to store all paths to leaf
-    
-    string output = "";  // to store individual path in a recursive call
-    
-    path( root , ans, output );
-    
-    return ans;
-}
 };
+
+ // */
+// class Solution {
+// public:
+//     void path(TreeNode* root , vector<string> &ans, string output ){
+// 		// base case
+// 		if( root == NULL ){
+// 			return;
+// 		}
+    
+//     // if  root is not null push the data into string
+//     output += to_string( root->val);
+    
+//     // if any left or right node of root exist push ->
+//     if( root->left || root->right ){
+//         output += "->";
+//     }
+    
+//     if( !root->left && !root->right ){
+//         ans.push_back( output); // store ans;
+//     }
+    
+    
+//     //leftside
+//     if(root->left)
+//         path(root->left , ans, output);
+    
+//     //rightside
+//     if(root->right)
+//         path(root->right , ans, output);
+    
+//     output.pop_back();  // backtrack
+    
+// }
+
+// vector<string> binaryTreePaths(TreeNode* root) {
+//     vector<string> ans;  // to store all paths to leaf
+    
+//     string output = "";  // to store individual path in a recursive call
+    
+//     path( root , ans, output );
+    
+//     return ans;
+// }
+// };
