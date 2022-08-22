a8d971fc6c93fc3c994c695020b927a7a6956031
Parija Annuprit <71126167+ParijaAnnuprit@users.noreply.github.com>
2022-08-22 17:49:54 +0530

    Time: 26 ms (46.35%), Space: 14.2 MB (85.74%) - LeetHub

--- 236-lowest-common-ancestor-of-a-binary-tree/236-lowest-common-ancestor-of-a-binary-tree.cpp
+++ 236-lowest-common-ancestor-of-a-binary-tree/236-lowest-common-ancestor-of-a-binary-tree.cpp
@@ -7,26 +7,38 @@
  *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
  * };
  */
+// class Solution {
+// public:
+//     TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+//         vector<TreeNode*>v1;
+//         vector<TreeNode*>v2;
+//         path(root,p,v1);
+//         path(root,q,v2);
+//         for(int i=v1.size()-1;i>=0;i--){
+//             for(int j=0;j<v2.size();j++){
+//                 if(v1[i]->val == v2[j]->val) return v1[i];
+//             }
+//         }
+//         return nullptr;
+//     }
+//     bool path(TreeNode* root, TreeNode* f, vector<TreeNode*> &v){
+//         if(root==NULL ) return false;
+//         v.push_back(root);
+//         if(root->val == f->val) return true;
+//         if(path(root->left,f,v) || path(root->right,f,v)) return true;
+//         v.pop_back();
+//         return false;
+//     }
+// };
+
 class Solution {
 public:
     TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
-        vector<TreeNode*>v1;
-        vector<TreeNode*>v2;
-        path(root,p,v1);
-        path(root,q,v2);
-        for(int i=v1.size()-1;i>=0;i--){
-            for(int j=0;j<v2.size();j++){
-                if(v1[i]->val == v2[j]->val) return v1[i];
-            }
-        }
-        return nullptr;
-    }
-    bool path(TreeNode* root, TreeNode* f, vector<TreeNode*> &v){
-        if(root==NULL ) return false;
-        v.push_back(root);
-        if(root->val == f->val) return true;
-        if(path(root->left,f,v) || path(root->right,f,v)) return true;
-        v.pop_back();
-        return false;
+        if(root==NULL || root==p || root==q) return root;
+        TreeNode* left = lowestCommonAncestor(root->left,p,q);
+        TreeNode* right = lowestCommonAncestor(root->right,p,q);
+        if(left==NULL) return right;
+        else if(right==NULL) return left;
+        return root;
     }
 };
