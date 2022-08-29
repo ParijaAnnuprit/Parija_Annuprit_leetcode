50ac33e658dc2d7a57ac1889a4c42392a05735ff
Parija Annuprit <71126167+ParijaAnnuprit@users.noreply.github.com>
2022-08-29 21:33:35 +0530

    Time: 287 ms (21.26%), Space: 45.4 MB (25.63%) - LeetHub

--- 912-sort-an-array/912-sort-an-array.cpp
+++ 912-sort-an-array/912-sort-an-array.cpp
@@ -1,30 +1,25 @@
 class Solution {
 public:
     vector<int> sortArray(vector<int>& nums) {
-//         heap sort.
+//         heap sort
         int n = nums.size();
-//         first sorting to array to find the last element
         for(int i=n/2-1;i>=0;i--){
             heapify(nums,n,i);
         }
         for(int i=n-1;i>0;i--){
-            swap(nums[0],nums[i]);
+            swap(nums[i],nums[0]);
             heapify(nums,i,0);
         }
         return nums;
     }
-    void heapify(vector<int>&nums, int n, int i){
+    void heapify(vector<int>& nums, int n, int i){
         int largest = i;
         int l = 2*i+1;
         int r = 2*i+2;
-        if(l<n && nums[largest]<nums[l]){
-            largest = l;
-        }
-        if(r<n && nums[largest]<nums[r]){
-            largest = r;
-        }
+        if(l<n && nums[largest]<nums[l]) largest = l;
+        if(r<n && nums[largest]<nums[r]) largest = r;
         if(largest != i){
-            swap(nums[i],nums[largest]);
+            swap(nums[largest],nums[i]);
             heapify(nums,n,largest);
         }
     }
