36416610e1bf8003f3c7fa3f1cb8e5f7dc9202f9
Parija Annuprit <71126167+ParijaAnnuprit@users.noreply.github.com>
2022-08-30 13:57:23 +0530

    Time: 213 ms (22.48%), Space: 45.5 MB (67.21%) - LeetHub

--- 215-kth-largest-element-in-an-array/215-kth-largest-element-in-an-array.cpp
+++ 215-kth-largest-element-in-an-array/215-kth-largest-element-in-an-array.cpp
@@ -26,19 +26,53 @@
 //         return nums[n-k];
 //     }
 // };
+
+
+// class Solution {
+// public:
+//     int findKthLargest(vector<int>& nums, int k) {
+// // priority queue
+//         priority_queue<int>q;
+//         for(int i=0;i<nums.size();i++){
+//             q.push(nums[i]);
+//         }
+//         int i=1;
+//         while(i<k){
+//             q.pop();
+//             ++i;
+//         }
+//         return q.top();
+//     }
+// };
+
+
 class Solution {
 public:
     int findKthLargest(vector<int>& nums, int k) {
-// priority queue
-        priority_queue<int>q;
-        for(int i=0;i<nums.size();i++){
-            q.push(nums[i]);
+//     heap sort
+        int n = nums.size();
+        for(int i=n/2-1; i>=0; i--){
+            heapify(nums,n,i);
         }
-        int i=1;
-        while(i<k){
-            q.pop();
-            ++i;
+        for(int i=n-1;i>0;i--){
+            swap(nums[i],nums[0]);
+            heapify(nums,i,0);
         }
-        return q.top();
+        int x;
+        for(int i=0;i<=n-k;i++){
+            x = nums[i];
+        }
+        return x;
+    }
+    void heapify(vector<int>&v, int n, int i){
+        int largest = i;
+        int l = 2*i+1;
+        int r = 2*i+2;
+        if(l<n && v[largest]<v[l]) largest = l;
+        if(r<n && v[largest]<v[r]) largest = r;
+        if(largest!=i){
+            swap(v[i],v[largest]);
+            heapify(v,n,largest);
+        }
     }
 };
