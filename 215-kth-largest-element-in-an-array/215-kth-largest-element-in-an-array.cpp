0ade1fedfea5289020ae122ce580ba28cb296c26
Parija Annuprit <71126167+ParijaAnnuprit@users.noreply.github.com>
2022-08-30 13:37:57 +0530

    Time: 149 ms (54.03%), Space: 49.9 MB (13.64%) - LeetHub

--- 215-kth-largest-element-in-an-array/215-kth-largest-element-in-an-array.cpp
+++ 215-kth-largest-element-in-an-array/215-kth-largest-element-in-an-array.cpp
@@ -1,17 +1,17 @@
-class Solution {
-public:
-    int findKthLargest(vector<int>& nums, int k) {
-//         bubble sort
-        int n = nums.size();
-        for(int i=0;i<k;i++){
-            for(int j=0;j<n-1;j++){
-                if(nums[j]>nums[j+1])
-                    swap(nums[j],nums[j+1]);
-            }
-        }
-        return nums[n-k];
-    }
-};
+// class Solution {
+// public:
+//     int findKthLargest(vector<int>& nums, int k) {
+// //         bubble sort
+//         int n = nums.size();
+//         for(int i=0;i<k;i++){
+//             for(int j=0;j<n-1;j++){
+//                 if(nums[j]>nums[j+1])
+//                     swap(nums[j],nums[j+1]);
+//             }
+//         }
+//         return nums[n-k];
+//     }
+// };
 // class Solution {
 // public:
 //     int findKthLargest(vector<int>& nums, int k) {
@@ -26,3 +26,19 @@ public:
 //         return nums[n-k];
 //     }
 // };
+class Solution {
+public:
+    int findKthLargest(vector<int>& nums, int k) {
+// priority queue
+        priority_queue<int>q;
+        for(int i=0;i<nums.size();i++){
+            q.push(nums[i]);
+        }
+        int i=1;
+        while(i<k){
+            q.pop();
+            ++i;
+        }
+        return q.top();
+    }
+};
