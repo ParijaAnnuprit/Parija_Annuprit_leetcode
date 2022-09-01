850255a86e59b7cba4e1dc068009d88e82501d02
Parija Annuprit <71126167+ParijaAnnuprit@users.noreply.github.com>
2022-09-01 23:10:46 +0530

    Time: 349 ms (5.02%), Space: 86.4 MB (54.67%) - LeetHub

--- 1448-count-good-nodes-in-binary-tree/1448-count-good-nodes-in-binary-tree.cpp
+++ 1448-count-good-nodes-in-binary-tree/1448-count-good-nodes-in-binary-tree.cpp
@@ -9,41 +9,41 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
-class Solution {
-public:
-    // int maxa = INT_MIN;
-    int count = 0;
-    int goodNodes(TreeNode* root) {
-        search(root, root->val);
-        return count;
-    }
-    void search(TreeNode* root , int currmax){
-        if(root ==NULL) return;
-        if(root->val >= currmax){
-            count++;
-            currmax = root->val;
-        }
-        search(root->left,currmax);
-        search(root->right,currmax);
-    }
-};
-
-
 // class Solution {
 // public:
+//     // int maxa = INT_MIN;
 //     int count = 0;
-//     void dfs(TreeNode* node,int curMax){
-//         if (!node) return;
-        
-//         if (node->val >= curMax){
-//             count++;
-//             curMax = node->val;
-//         }
-//         dfs(node->left, curMax);
-//         dfs(node->right, curMax);
-//     }
 //     int goodNodes(TreeNode* root) {
-//         dfs(root, root->val);
+//         search(root, root->val);
 //         return count;
 //     }
+//     void search(TreeNode* root , int currmax){
+//         if(root ==NULL) return;
+//         if(root->val >= currmax){
+//             count++;
+//             currmax = root->val;
+//         }
+//         search(root->left,currmax);
+//         search(root->right,currmax);
+//     }
 // };
+
+
+class Solution {
+public:
+    int count = 0;
+    void dfs(TreeNode* node,int curMax){
+        if (!node) return;
+        
+        if (node->val >= curMax){
+            count++;
+            curMax = node->val;
+        }
+        dfs(node->left, curMax);
+        dfs(node->right, curMax);
+    }
+    int goodNodes(TreeNode* root) {
+        dfs(root, root->val);
+        return count;
+    }
+};
