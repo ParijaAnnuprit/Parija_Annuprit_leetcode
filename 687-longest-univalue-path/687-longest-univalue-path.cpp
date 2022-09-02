2d6a9ac70fa5d5bef1367927c9d797b4d67a7da9
Parija Annuprit <71126167+ParijaAnnuprit@users.noreply.github.com>
2022-09-02 20:51:07 +0530

    Time: 209 ms (55.68%), Space: 71.8 MB (49.33%) - LeetHub

--- 687-longest-univalue-path/687-longest-univalue-path.cpp
+++ 687-longest-univalue-path/687-longest-univalue-path.cpp
@@ -11,60 +11,37 @@
  */
 class Solution {
 public:
-    int maxa = 0;
+    int maxa =0;
     int longestUnivaluePath(TreeNode* root) {
-        if(!root) return 0;
-        helper(root,INT_MIN);
+        maxpath(root);
         return maxa;
     }
-    int helper(TreeNode* root, int parentvalue){
-        if(root == nullptr) return 0;
-        int leftcounter = helper(root->left,root->val);
-        int rightcounter = helper(root->right,root->val);
-        maxa = max(leftcounter+rightcounter, maxa);
-        if(root->val == parentvalue) return max(leftcounter,rightcounter)+1;
+    int maxpath(TreeNode* root){
+        if(root==nullptr) return 0;
+        int left = maxpath(root->left);
+        int right = maxpath(root->right);
+        bool flagl = false;
+        bool flagr = false;
+        if(root->left && root->val == root->left->val){
+            left = left+1;
+            flagl= true;
+        }
+        if(root->right && root->val == root->right->val){
+            right = right+1;
+            flagr = true;
+        }
+        if(flagl && flagr){
+            maxa = max(maxa,left+right);
+            return max(left,right);
+        }
+        else if(flagl){
+            maxa = max(maxa,left);
+            return left;
+        }
+        else if(flagr){
+            maxa = max(maxa,right);
+            return right;
+        }
         return 0;
     }
 };
-
-
-// class Solution {
-// public:
-
-// //initialize global counters with initial values of zeros
-// int maxCounter=0;
-
-// int helper(TreeNode *root, int parentValue){
-    
-//     //if there's no tree-> stop
-//     if(!root)
-//         return 0;
-    
-//     //recursively call the helper function with the left nodes
-//     int leftCounter=helper(root->left, root->val);
-//     //recursively call the helper function with the right nodes
-//     int rightCounter=helper(root->right, root->val);
-    
-//     //save the maximum available counter 
-//     maxCounter=max(leftCounter + rightCounter , maxCounter);
-    
-//     //check if the parent node value equals the current, left or right, node value-> add the largest value from the two counters by one
-//     if(root->val==parentValue)
-//         return max(leftCounter, rightCounter)+1;
-        
-//     return 0;
-// }
-
-// int longestUnivaluePath(TreeNode* root) {
-    
-//     //if there's no tree-> retrun 0
-//     if(!root)
-//         return 0;
-    
-//     //call the helper function with the main root node & a value without the range, as we started from -1000
-//     helper(root, INT_MIN);
-
-//     //return the maximum counter value
-//     return maxCounter;
-// }
-// };
