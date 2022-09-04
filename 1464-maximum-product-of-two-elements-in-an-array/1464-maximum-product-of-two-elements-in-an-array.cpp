9fad8630c893766571eac9dd3e50c476e63a926f
Parija Annuprit <71126167+ParijaAnnuprit@users.noreply.github.com>
2022-09-04 23:51:48 +0530

    Time: 19 ms (13.44%), Space: 10.1 MB (25.33%) - LeetHub

--- 1464-maximum-product-of-two-elements-in-an-array/1464-maximum-product-of-two-elements-in-an-array.cpp
+++ 1464-maximum-product-of-two-elements-in-an-array/1464-maximum-product-of-two-elements-in-an-array.cpp
@@ -1,12 +1,26 @@
 class Solution {
 public:
     int maxProduct(vector<int>& nums) {
-        priority_queue<int>q;
-        for(auto i:nums){
-            q.push(i);
+//         heap sort
+        int n = nums.size();
+        for(int i=n/2;i>=0;i--){
+            heapify(nums,i,n);
         }
-        int x = q.top()-1;
-        q.pop();
-        return x*(q.top()-1);
+        for(int i=n-1;i>0;i--){
+            swap(nums[i],nums[0]);
+            heapify(nums,0,i);
+        }
+        return (nums[nums.size()-1]-1)*(nums[nums.size()-2]-1);
+    }
+    void heapify(vector<int>& v, int i, int n){
+        int largest = i;
+        int l = 2*i+1;
+        int r = 2*i+2;
+        if(l<n && v[largest]<v[l]) largest = l;
+        if(r<n && v[largest]<v[r]) largest = r;
+        if(largest!=i){
+            swap(v[i],v[largest]);
+            heapify(v,largest,n);
+        }
     }
 };
