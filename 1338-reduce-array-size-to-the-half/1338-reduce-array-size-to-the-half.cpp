ee54c1bbdcc290567b1aa00b96c881d172b287ec
Parija Annuprit <71126167+ParijaAnnuprit@users.noreply.github.com>
2022-09-04 15:46:30 +0530

    Time: 429 ms (23.47%), Space: 78.3 MB (74.75%) - LeetHub

--- 1338-reduce-array-size-to-the-half/1338-reduce-array-size-to-the-half.cpp
+++ 1338-reduce-array-size-to-the-half/1338-reduce-array-size-to-the-half.cpp
@@ -1,23 +1,45 @@
+// class Solution {
+// public:
+//     int minSetSize(vector<int>& arr) {
+//         int n = arr.size();
+//         int x = n;
+//         int count=0;
+//         unordered_map<int,int>mpp;
+//         priority_queue<pair<int,int>>q;
+//         for(auto i:arr){
+//             mpp[i]++;
+//         }
+//         for(auto i:mpp){
+//             q.push({i.second,i.first});
+//         }
+//         while(!q.empty()){
+//             x-=q.top().first;
+//             count++;
+//             if(x<=n/2) break;
+//             else q.pop();
+//         }
+//         return count;
+//     }
+// };
+
 class Solution {
 public:
     int minSetSize(vector<int>& arr) {
-        int n = arr.size();
-        int x = n;
+        int n=arr.size();
         int count=0;
-        unordered_map<int,int>mpp;
-        priority_queue<pair<int,int>>q;
-        for(auto i:arr){
-            mpp[i]++;
+        int res=0;
+        unordered_map<int,int> hash;
+        priority_queue<int> pq;
+        for(auto i:arr)
+            hash[i]++;  
+        for(auto i:hash)
+            pq.push(i.second);
+        while(!pq.empty()){
+            if(count>=n/2)break;               
+            count+=pq.top();
+            pq.pop();
+            res++;
         }
-        for(auto i:mpp){
-            q.push({i.second,i.first});
-        }
-        while(!q.empty()){
-            x-=q.top().first;
-            count++;
-            if(x<=n/2) break;
-            else q.pop();
-        }
-        return count;
+        return res;
     }
 };
