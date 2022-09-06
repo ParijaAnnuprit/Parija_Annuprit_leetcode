7c48db4ddcb07c2602fe46e5f01077b92d80d64d
Parija Annuprit <71126167+ParijaAnnuprit@users.noreply.github.com>
2022-09-06 15:25:58 +0530

    Time: 142 ms (22.90%), Space: 10.7 MB (23.78%) - LeetHub

--- 264-ugly-number-ii/264-ugly-number-ii.cpp
+++ 264-ugly-number-ii/264-ugly-number-ii.cpp
@@ -1,39 +1,39 @@
-// class Solution {
-// public:
-//     int nthUglyNumber(int n) {
-//         priority_queue<int,vector<int>,greater<int>>q;
-//         q.push(1);
-//         n--;
-//         while(n){
-//             long long int k = q.top();
-//             q.push(k*2);
-//             q.push(k*3);
-//             q.push(k*5);
-//             q.pop();
-//             while(q.top()==k) q.pop();
-//             n--;
-//         }
-//         return q.top();
-//     }
-// };
-
-
 class Solution {
 public:
     int nthUglyNumber(int n) {
-        priority_queue<long long int ,vector<long long int>,greater<long long int>> pq;
-        pq.push(1);
+        priority_queue<long long int,vector<long long int>,greater<long long int>>q;
+        q.push(1);
         n--;
-        while(n>0)
-        {
-            long long int k=pq.top();
-            pq.push(k*2);
-            pq.push(k*3);
-            pq.push(k*5);
-            pq.pop();
-            while(pq.top()==k) pq.pop();
+        while(n>0){
+            long long int k = q.top();
+            q.push(k*2);
+            q.push(k*3);
+            q.push(k*5);
+            q.pop();
+            while(q.top()==k) q.pop();
             n--;
         }
-        return pq.top();
+        return q.top();
     }
 };
+
+
+// class Solution {
+// public:
+//     int nthUglyNumber(int n) {
+//         priority_queue<long long int ,vector<long long int>,greater<long long int>> pq;
+//         pq.push(1);
+//         n--;
+//         while(n>0)
+//         {
+//             long long int k=pq.top();
+//             pq.push(k*2);
+//             pq.push(k*3);
+//             pq.push(k*5);
+//             pq.pop();
+//             while(pq.top()==k) pq.pop();
+//             n--;
+//         }
+//         return pq.top();
+//     }
+// };
