1aa57ed99f181ca6a1423e8920aab11c8528fc00
Parija Annuprit <71126167+ParijaAnnuprit@users.noreply.github.com>
2022-09-07 16:18:22 +0530

    Time: 847 ms (61.45%), Space: 167.1 MB (74.64%) - LeetHub

--- 2349-design-a-number-container-system/2349-design-a-number-container-system.cpp
+++ 2349-design-a-number-container-system/2349-design-a-number-container-system.cpp
@@ -1,31 +1,27 @@
-// unordered_map<int,set<int>>mnum;
-// unordered_map<int,int>mind;
 
-// class NumberContainers {
-// public:
-//     NumberContainers() {
+
+
+class NumberContainers {
+public:
+    unordered_map<int,priority_queue<int,vector<int>, greater<int>>>mnum;
+    unordered_map<int,int>mind;
+    NumberContainers() {
         
-//     }
+    }
     
-//     void change(int index, int number) {
-//         if(mind.find(index)==mind.end()){
-//             mind[index]=number;
-//             mnum[number].insert(index);
-//         }
-//         else if(mind.find(index)!=mind.end()){
-//             int x = mind[index];
-//             mnum[x].erase(index);
-//             mind[index]=number;
-//             mnum[number].insert(index);
-//         }
-//     }
+    void change(int index, int number) {
+        mind[index]=number;
+        mnum[number].push(index);
+    }
     
-//     int find(int number) {
-//         if(mnum.find(number)==mnum.end())
-//             return -1;
-//         return *mnum[number].begin();
-//     }
-// };
+    int find(int number) {
+        while(mnum[number].size()){
+            if(mind[mnum[number].top()]==number) return mnum[number].top();
+            else mnum[number].pop();
+        }
+        return -1;
+    }
+};
 
 /**
  * Your NumberContainers object will be instantiated and called as such:
@@ -33,31 +29,3 @@
  * obj->change(index,number);
  * int param_2 = obj->find(number);
  */
-
-
-class NumberContainers {
-public:
-    unordered_map<int,set<int>>map_nums; //(number,{index}) (used set in place of unordered set to get minimum quickly )
-    unordered_map<int,int>map_index;// (index,number)
-    NumberContainers() {
-        
-    }
-    void change(int index, int number) {
-         //if index already exists we have to change the map_index at given "index"
-         if(map_index.find(index)!=map_index.end())
-         {
-             int num=map_index[index];  
-             if(map_nums[num].size()) map_nums[num].erase(index);
-             if(map_nums[num].size()==0)
-                 map_nums.erase(num);
-         }
-          map_index[index]=number;
-            map_nums[number].insert(index);    
-    }
-    
-    int find(int number) {
-        if(map_nums.find(number)==map_nums.end())
-            return -1;
-        return *map_nums[number].begin();
-    }
-};
