6ce70fb33b1c296f550e88021b044c769197f5d0
Parija Annuprit <71126167+ParijaAnnuprit@users.noreply.github.com>
2022-09-09 12:13:50 +0530

    Time: 1465 ms (5.07%), Space: 99.2 MB (87.99%) - LeetHub

---
+++ 1738-find-kth-largest-xor-coordinate-value/1738-find-kth-largest-xor-coordinate-value.cpp
@@ -0,0 +1,51 @@
+// class Solution {
+// public:
+//     int kthLargestValue(vector<vector<int>>& matrix, int k) {
+//         int m = matrix.size();
+//         int n = matrix[0].size();
+//         priority_queue<int>q;
+//         for(int i=0;i<m;i++){
+//             for(int j=0;j<n;j++){
+//                 if(i==0 || j==0){
+//                     if(i==0 && j==0) q.push(matrix[i][j]);
+//                     else if(i!=0) q.push(matrix[i][j]^matrix[i-1][j]);
+//                     else if(j!=0) q.push(matrix[i][j]^matrix[i][j-1]);
+//                 }
+//                 else{
+//                     q.push(matrix[i][j]^matrix[i-1][j]^matrix[i][j-1]^matrix[i-1][j-1]);
+//                 }
+//             }
+//         }
+//         while(k>1){
+//             q.pop();
+//             k--;
+//         }
+//         return q.top();
+//     }
+// };
+
+class Solution {
+public:
+    int kthLargestValue(vector<vector<int>>& matrix, int k) {
+        priority_queue<int,vector<int>, greater<int>> pq;
+        
+        for(int i = 0 ; i < matrix.size(); i++)
+        {
+            for(int j = 0; j < matrix[0].size(); j++)
+            {
+                if(i > 0 && j > 0)  
+                    matrix[i][j] = matrix[i][j] ^ matrix[i-1][j] ^ matrix[i][j-1] ^ matrix[i-1][j-1];
+                else if(i > 0) 
+                    matrix[i][j] = matrix[i-1][j] ^ matrix[i][j] ;
+                else if(j > 0)  
+                    matrix[i][j] = matrix[i][j-1] ^ matrix[i][j] ;
+
+                pq.push(matrix[i][j]);
+                
+                if(pq.size() > k)
+                    pq.pop();
+            }
+        }
+        return pq.top();
+    }
+};
