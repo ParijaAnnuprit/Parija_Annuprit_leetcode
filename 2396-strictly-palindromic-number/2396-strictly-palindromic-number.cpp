787f5e0e3ba114d19c64f11b2d9170e25b7b7afa
Parija Annuprit <71126167+ParijaAnnuprit@users.noreply.github.com>
2022-09-09 17:59:24 +0530

    Time: 2 ms (46.12%), Space: 5.8 MB (77.69%) - LeetHub

--- 2396-strictly-palindromic-number/2396-strictly-palindromic-number.cpp
+++ 2396-strictly-palindromic-number/2396-strictly-palindromic-number.cpp
@@ -1,51 +1,53 @@
-// class Solution {
-// public:
-//     bool isStrictlyPalindromic(int n) {
-//         string base = "";
-//         int temp = n;
-//         for(int i=2;i<=n-2;i++){
-//             while(temp){
-//                 base = base + to_string(temp%i);
-//                 temp = temp/i;
-//             }
-// //             for palindrome
-//             for(int j=0;j<base.length();j++){
-//                 if(base[j]!=base[base.length()-j+1]) return false;
-//                 // else j++;
-//             }
-//             base.clear();
-//         }
-//         return true;
-//     }
-// };
-
-
-class Solution 
-{
+class Solution {
 public:
-    bool isStrictlyPalindromic(int n) 
-    {
-        int temp;
+    bool isStrictlyPalindromic(int n) {
         string base;
-        for(int i=2; i<=n-2; i++)
-        {
-            temp=n;
-            while(temp)
-            {
-                base = to_string(temp%i) + base;
+        int temp;
+        for(int i=2;i<=n-2;i++){
+            temp = n;
+            while(temp){
+                base = base + to_string(temp%i);
                 temp = temp/i;
             }
-        
-            int sz=base.size();
-            for(int j=0; j<sz; j++)
-            {
-                if(base[j]!=base[sz-j-1])
-                    return false;    
-                
+//             for palindrome
+            int s = base.size();
+            for(int j=0;j<s;j++){
+                if(base[j]!=base[s-j-1]) return false;
+                // else j++;
             }
             base.clear();
         }
         return true;
-        
     }
 };
+
+
+// class Solution 
+// {
+// public:
+//     bool isStrictlyPalindromic(int n) 
+//     {
+//         int temp;
+//         string base;
+//         for(int i=2; i<=n-2; i++)
+//         {
+//             temp=n;
+//             while(temp)
+//             {
+//                 base = to_string(temp%i) + base;
+//                 temp = temp/i;
+//             }
+        
+//             int sz=base.size();
+//             for(int j=0; j<sz; j++)
+//             {
+//                 if(base[j]!=base[sz-j-1])
+//                     return false;    
+                
+//             }
+//             base.clear();
+//         }
+//         return true;
+        
+//     }
+// };
