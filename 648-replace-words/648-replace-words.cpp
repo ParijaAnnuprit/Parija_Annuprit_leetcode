72fc534d95201b9c104492a1bd8072c6bd1337e0
Parija Annuprit <71126167+ParijaAnnuprit@users.noreply.github.com>
2022-09-23 02:11:08 +0530

    Time: 188 ms (59.72%), Space: 79.8 MB (32.30%) - LeetHub

--- 648-replace-words/648-replace-words.cpp
+++ 648-replace-words/648-replace-words.cpp
@@ -1,70 +1,82 @@
-// class Solution {
-// public:
-//     string replaceWords(vector<string>& dictionary, string sentence) {
-        
-//     }
-// };
+struct Node{
+    Node* arr[26];
+    int flag = false;
+    bool containskey(char ch){
+        return (arr[ch-'a']!=NULL);
+    }
+    void put(char ch, Node* node){
+        arr[ch-'a'] = node;
+    }
+    Node* next(char ch){
+        return arr[ch-'a'];
+    }
+    void setend(){
+        flag = true;
+    }
+    bool retend(){
+        return flag;
+    }
+};
+
+
+
 class Solution {
 public:
     string replaceWords(vector<string>& dictionary, string sentence) {
-        unordered_map<int, string> hashes;
-        int n = dictionary.size(), base = 27;
-        vector<string> words;
-        long long int prime = 1000000007 , maxBase = 1, currentHash = 0;
-        string word = "";
-		//from here we start step1.
-        for(int i = 0; i < n; i++){
-            currentHash = 0;
-            word = dictionary[i];
-            int m = word.size();
-            for(int j = 0 ; j < m; j++){
-                currentHash = currentHash*base % prime;
-                currentHash += word[j] - 'a' + 1;
-            }
-            
-            hashes[currentHash] = word;
+        Node* root = new Node();
+        for(int i=0;i<dictionary.size();i++){
+            insert(dictionary[i], root);
         }
-		//step 1 ends here
-        string answer = "";
-        int start = 0;
-        currentHash = 0;
-        for(int i = 0; i < sentence.size(); i++){
-            if(sentence[i] != ' '){
-                currentHash  = currentHash*base % prime;
-                currentHash += sentence[i] - 'a' + 1;
-                if(hashes.find(currentHash) != hashes.end()){
-				//we replace or append as stated in step 3 and jump on to the next word in sentence using while loop
-                    answer = answer + hashes[currentHash];
-                    answer += ' ';
-                    currentHash = 0;
-                    while(i < sentence.size()){
-                        if(sentence[i] != ' '){
-                            i++;
-                        }
-                        else{
-                            break;
-                        }
-                    }
-                    start = i+1;
-                }
-            }
-            else{
-                currentHash = 0;
-              answer += sentence.substr(start , i - start);
-                start = i+1;
-                answer  += ' ';
-            }    
-			// this statement is just for an edge case 
-           if(i == sentence.size()-1){
-               for(int i = start ; i < sentence.size(); i++){
-                   answer += sentence[i];
-               }   
-                answer += ' ';
-            }
+        queue<string>q,nq;
+        map<string,string>mpp;
+        stringstream x(sentence);
+        string t;
+        string res;
+        while(getline(x,t,' ')){
+            q.push(t);
         }
-		//we pop out the additional space added from our answer string.
-        answer.pop_back();
-        return answer;
-    
+        while(!q.empty()){
+            string k = q.front();
+            q.pop();
+            nq.push(k);
+            string j = search(k, root);
+            if(j.length()==0) mpp[k] = k;
+            else mpp[k] = j; 
+        }
+        while(!nq.empty()){
+            res+= mpp[nq.front()];
+            nq.pop();
+            res+=' ';
+        }
+        res.pop_back();
+        return res;
+    }
+//     inserting strings inside the trie
+    void insert(string s, Node* root){
+        Node* dummy = root;
+        for(int i=0;i<s.length();i++){
+            if(!dummy->containskey(s[i])){
+                dummy->put(s[i], new Node());
+            }
+            dummy = dummy->next(s[i]);
+        }
+        dummy->setend();
+    }
+//     searching strings inside the trie
+    string search(string s, Node* root){
+        Node* dummy = root;
+        string k = "";
+        for(int i=0;i<s.length();i++){
+            if(dummy->retend()== true)
+                return k;
+            else if(dummy->containskey(s[i])){
+                k+=s[i];
+                dummy = dummy->next(s[i]);
+            }
+            else break;
+        }
+        return "";
     }
 };
+
+// !dummy->containskey(s[i]) && 
