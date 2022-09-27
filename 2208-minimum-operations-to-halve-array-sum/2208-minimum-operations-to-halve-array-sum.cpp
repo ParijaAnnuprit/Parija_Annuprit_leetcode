d6bd71099016b4557ac72fff98e92cc27fd35784
Parija Annuprit <71126167+ParijaAnnuprit@users.noreply.github.com>
2022-09-27 20:11:13 +0530

    Time: 441 ms (54.21%), Space: 89.5 MB (6.15%) - LeetHub

---
+++ 2208-minimum-operations-to-halve-array-sum/2208-minimum-operations-to-halve-array-sum.cpp
@@ -0,0 +1,51 @@
+// class Solution {
+// public:
+//     int halveArray(vector<int>& nums) {
+//         float sum = 0;
+//         int count =0;
+//         priority_queue<float>q;
+//         for(int i=0;i<nums.size();i++){
+//             float a = nums[i];
+//             q.push(a);
+//             sum+=a;
+//         }
+//         float half = sum/2;
+//         while(!q.empty()){
+//             float b = q.top()/2.0;
+//             sum-=q.top();
+//             sum+=b;
+//             q.pop();
+//             q.push(b);
+//             count++;
+//             if(sum<=half) break;
+//         }
+//         return count;
+//     }
+// };
+
+
+
+class Solution {
+public:
+    int halveArray(vector<int>& nums) {
+        double sum=0,s=0,count=0;
+        priority_queue<double> pq;
+        for(int i=0;i<nums.size();i++)
+        {
+            sum+=nums[i];
+            s+=nums[i];
+            pq.push(nums[i]);
+        }
+        while(!pq.empty() && s>sum/2)
+        {
+            double x=pq.top();
+            pq.pop();
+            x/=2.0;
+            s-=x;
+            pq.push(x);
+            count++;
+        }
+        return count;
+        
+    }
+};
