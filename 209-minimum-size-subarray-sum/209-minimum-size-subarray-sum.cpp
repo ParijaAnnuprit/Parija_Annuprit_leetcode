0f08a0ff808d3c05f5da3d5afb0afbea169f9aea
Parija Annuprit <71126167+ParijaAnnuprit@users.noreply.github.com>
2022-10-03 22:30:01 +0530

    Time: 158 ms (5.09%), Space: 28.1 MB (88.48%) - LeetHub

--- 209-minimum-size-subarray-sum/209-minimum-size-subarray-sum.cpp
+++ 209-minimum-size-subarray-sum/209-minimum-size-subarray-sum.cpp
@@ -20,3 +20,24 @@ public:
         return mini;
     }
 };
+
+
+// class Solution {
+// public:
+//     int minSubArrayLen(int target, vector<int>& nums) {
+//         int mini = INT_MAX;
+//         for(int i=0;i<nums.size();i++){
+//             int j=i;
+//             int sum = 0;
+//             int count =0;
+//             while(j<nums.size() && sum<target){
+//                 sum = sum+nums[j];
+//                 count++;
+//                 j++;
+//             }
+//             if(sum>=target) mini = min(mini, count);
+//         }
+//         if(mini == INT_MAX) return 0;
+//         return mini;
+//     }
+// };
