8120d24b911c1b23038852759be3c05d55eb760d
Parija Annuprit <71126167+ParijaAnnuprit@users.noreply.github.com>
2022-10-04 18:17:15 +0530

    Time: 978 ms (95.14%), Space: 93.6 MB (41.34%) - LeetHub

--- 1792-maximum-average-pass-ratio/1792-maximum-average-pass-ratio.cpp
+++ 1792-maximum-average-pass-ratio/1792-maximum-average-pass-ratio.cpp
@@ -1,72 +1,28 @@
-// class Solution {
-// public:
-//     double maxAverageRatio(vector<vector<int>>& classes, int extraStudents) {\
-//         double deltasum=0;
-//         priority_queue<pair<double,pair<int,int>>>q;
-//         for(int i=0;i<classes.size();i++){
-//             int pass = classes[i][0];
-//             int total = classes[i][1];
-//             deltasum += (double) pass/total;
-//             double delta = (double)(pass+1)/(total+1) - (double)(pass)/(total);
-//             q.push({delta,{++pass,++total}});
-//         }
-//         while(extraStudents--){
-//             int pass = q.top().second.first;
-//             int total = q.top().second.second;
-//             deltasum += q.top().first;
-//             double newdelta = (double)(pass+1)/(total+1) - (double)(pass)/(total);
-//             q.pop();
-//             q.push({newdelta,{++pass,++total}});
-//         }
-//         return deltasum;
-                                                                             
-//     }
-// };
-
-
-struct klass {
-    int pass;
-    int total;
-    double ratio;
-    klass() {}
-    klass(int pass, int total) {
-        this->pass = pass;
-        this->total = total;
-        update_ratio();
-    }
-    void update_ratio() { ratio = pass * 1.0 / total; }
-    void increase(int by = 1) {
-        pass += by;
-        total += by;
-        update_ratio();
-    }
-    double delta() {
-        return (pass + 1) * 1.0 / (total + 1) - (pass)*1.0 / (total);
-    }
-    bool operator()(klass &a, klass &b) { return a.delta() < b.delta(); }
-};
-
 class Solution {
-   public:
-    double maxAverageRatio(vector<vector<int>> &classes, int extraStudents) {
-        priority_queue<klass, vector<klass>, klass> pq;
-        int total = classes.size();
-        double sum = 0;
-        klass k;
-        for (auto i : classes) {
-            k = klass(i[0], i[1]);
-            pq.push(k);
+public:
+    double maxAverageRatio(vector<vector<int>>& classes, int extraStudents) {
+        double cls = classes.size();
+        double deltasum=0;
+        priority_queue<pair<double,pair<int,int>>>q;
+        for(int i=0;i<classes.size();i++){
+            int pass = classes[i][0];
+            int total = classes[i][1];
+            deltasum += (double) pass/total;
+            double delta = (double)(pass+1)/(total+1) - (double)(pass)/(total);
+            q.push({delta,{++pass,++total}});
         }
-        while (extraStudents--) {
-            k = pq.top();
-            pq.pop();
-            k.increase();
-            pq.push(k);
+        while(extraStudents--){
+            int pass = q.top().second.first;
+            int total = q.top().second.second;
+            deltasum += q.top().first;
+            double newdelta = (double)(pass+1)/(total+1) - (double)(pass)/(total);
+            q.pop();
+            q.push({newdelta,{++pass,++total}});
         }
-        while (pq.size()) {
-            sum += pq.top().ratio;
-            pq.pop();
-        }
-        return sum / total;
+        
+        return deltasum/cls;
+                                                                             
     }
 };
+
+
